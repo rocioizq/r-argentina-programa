6e17b2a5a49ffd451be6bc4454e27d23040e3f0d
Rocio <53886101+rocioizq@users.noreply.github.com>
2024-02-16 16:17:06 +0100

    Update tarea-clase-4.js

--- clase-4/tarea-clase-4.js
+++ clase-4/tarea-clase-4.js
@@ -36,7 +36,7 @@ for (i = 1; i <= 50; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
 
-    } else if (i & 3 === 0) {
+    } else if (i % 3 === 0) {
         console.log("Fizz");
 
     } else if (i % 5 === 0) {
